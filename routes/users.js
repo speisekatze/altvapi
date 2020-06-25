d31f763f6dbe65e1959e9dd60b26eac338dc83ce
Thomas Walter <speisekatze@users.noreply.github.com>
2020-06-25 23:35:06 +0200

    put route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	connection.query('SELECT * from users', function (error, results, fields) {
+	connection.query('SELECT * from accounts', function (error, results, fields) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 	  		//If there is error, we send the error in the error section with 500 status
@@ -12,6 +12,10 @@ router.get('/', function(req, res, next) {
   			//If there is no error, all is good and response is 200OK.
 	  	}
   	});
+});
+
+router.put('/:id', (req, res) => {
+  res.send(JSON.stringify({"given": req.params.id}));
 });
 
 module.exports = router;
